5c37deb634df087977d34dd59bc781dca976796c
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2013-11-12 19:41:15 +0100

    Created a menu view

--- src/com/mrk1869/glasslogger/MainActivity.java
+++ src/com/mrk1869/glasslogger/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mrk1869.glasslogger;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.app.Activity;
 import android.content.Context;
@@ -10,45 +15,98 @@ import android.content.Intent;
 
 public class MainActivity extends Activity{
 
-    private Button startButton;
-    private Button monitoringButton;
     private Context mContext;
     LoggerService serviceBinder;
     IRSensorLogger irSensorLogger;
+    
+    private boolean mJustSelected;
+    private View backgroundView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LinearLayout layout = new LinearLayout(this);
+        setContentView(layout);
+        backgroundView =  new View(this);
+        layout.addView(backgroundView);
         mContext = getBaseContext();
-
-        startButton = (Button)findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isServiceRunning()) {
-                    stopRecording();
-                }else{
-                    startRecording();
-                }
-            }
-        });
-        
-        monitoringButton = (Button)findViewById(R.id.monitoring_button);
-        monitoringButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MonitoringActivity.class);
-                startActivity(intent);                                
-            }
-        });
     }
+    
+    @Override
+    protected void onResume(){
+        super.onResume();
+        openOptionsMenu();
+    }
+    
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        super.onPrepareOptionsMenu(menu);
+        MenuItem startMenuItem = (MenuItem)menu.findItem(R.id.start_recording);
+        MenuItem stopMenuItem = (MenuItem)menu.findItem(R.id.stop_recording);
+        if (isServiceRunning()) {
+            startMenuItem.setVisible(false);
+            stopMenuItem.setVisible(true);
+            backgroundView.setBackgroundColor(0xffcd2e2e);
+        }else{
+            startMenuItem.setVisible(true);
+            stopMenuItem.setVisible(false);
+            backgroundView.setBackgroundColor(0xff000000);
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.start_recording:
+                startRecording();
+                backgroundView.setBackgroundColor(0xffcd2e2e);
+                mJustSelected = true;
+                return false;
+            case R.id.stop_recording:
+                backgroundView.setBackgroundColor(0xff000000);
+                stopRecording();
+                mJustSelected = true;
+                return false;
+            case R.id.monitoring:
+                Intent intent = new Intent(MainActivity.this, MonitoringActivity.class);
+                startActivity(intent);
+                mJustSelected = false;
+                return false;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    public void onOptionsMenuClosed(Menu menu) {
+        if (mJustSelected) {
+            //FIXME: need to wait a bit
+            Handler h = new Handler(Looper.getMainLooper());
+            h.post(new Runnable() {
+                @Override
+                public void run() {
+                    openOptionsMenu();
+                }
+            });
+            mJustSelected = false;
+        } else {
+//            User dismissed so back out completely
+//            FIXME: right now, calling finish here doesn't seem to let us re-enable the receiver
+            finish();
+        }
+    }
+    
     private void startRecording(){
         if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
             Intent bindIndent = new Intent(MainActivity.this, LoggerService.class);
             mContext.startService(bindIndent);
-            startButton.setText("Stop recording");
-            startButton.setTextColor(0xffcd2e2e);
         }else{
             Toast.makeText(getBaseContext(), "Error: Storage does not have enough space.", Toast.LENGTH_SHORT).show();
         }
@@ -57,8 +115,6 @@ public class MainActivity extends Activity{
     private void stopRecording(){
         Intent bindIndent = new Intent(MainActivity.this, LoggerService.class);
         mContext.stopService(bindIndent);
-        startButton.setText("Start recording");
-        startButton.setTextColor(0xffffffff);
     }
 
     private boolean isServiceRunning(){
